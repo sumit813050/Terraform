6f074f3304790183bbdf3df3d27d248623db61d6
sumit813050 <tyagii.sumit07@gmail.com>
2025-06-07 02:11:04 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+	required_providers {
+		aws= {
+			source="hashicorp/aws"
+			version="~> 4.16"
+			}
+		azurerm = {
+			 source  = "hashicorp/azurerm"
+     			 version = "~> 3.0.2"
+   			 }
+		}
+	required_version = ">= 1.2.0"
+	}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+	
